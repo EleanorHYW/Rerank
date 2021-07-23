f0a28b170e34e2c97bbad01f01272d32ecbbac0b
eleanorhyw <eleanorhyw@gmail.com>
2021-07-23 18:51:49 +0800

    uodate run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
-CUDA_VISIBLE_DEVICES=0 python Train.py --train_size 0.98 --valid_size 0.01 \
---eval_size 0.01 --batch_size 128 --n_epochs 10 --gpu --max_len 500 \
---embedding_size 136 --hiddens 128 --n_lstms 1 --dropout 0.1 --L2 \
---ckpt_file ./models/base_bsz_128_h_128_dp_0.1_L2_1_ckp_0.pt \
+
+echo "start training seq2slate with batch size $1, hidden dimension $2, dropout $3 and L2 $4"
+
+
+if [ $4 -eq 1 ]; then
+    CUDA_VISIBLE_DEVICES=0 python Train.py --train_size 0.98 --valid_size 0.01 \
+    --eval_size 0.01 --batch_size $1 --n_epochs 10 --gpu --max_len 500 \
+    --embedding_size 136 --hiddens $2 --n_lstms 1 --dropout $3 --L2 \
+    --ckpt_file ./models/base_bsz_$1_h_$2_dp_$3_L2_$4_ckp_0.pt 
+else
+    CUDA_VISIBLE_DEVICES=0 python Train.py --train_size 0.98 --valid_size 0.01 \
+    --eval_size 0.01 --batch_size $1 --n_epochs 10 --gpu --max_len 500 \
+    --embedding_size 136 --hiddens $2 --n_lstms 1 --dropout $3 \
+    --ckpt_file ./models/base_bsz_$1_h_$2_dp_$3_L2_$4_ckp_0.pt 
+fi
